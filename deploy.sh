106dd7fdedb2cfb326b35a7a06711233a546d3b6
Hayden paul <103825348+Haydenpal@users.noreply.github.com>
2023-11-21 09:37:39 +0530

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # Set your AWS region and account ID
-AWS_REGION="ap-south-1"  # Change this to your preferred AWS region
+AWS_REGION="us-east-1"  # Change this to your preferred AWS region
 AWS_ACCOUNT_ID="137543238908"
 ECR_REPO_NAME="my-ecr-repo"  # Change this to your desired repository name
 
